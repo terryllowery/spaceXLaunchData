2f09a3a36fb7d580533562e777b3f326123931c5
Terry Lowery <tl@lowerytech.net>
2021-02-08 11:04:20 -0600

    adding loop to go through launch data

--- mod.ts
+++ mod.ts
@@ -1,4 +1,13 @@
+import { lastIndexOf } from "https://deno.land/std@0.86.0/bytes/mod.ts";
 import {log} from './deps.ts';
+
+interface Launch {
+  flightNumber: number;
+  mission: string;
+}
+
+const launches = new Map<number, Launch>();
+
 
 async function downloadLaunchData() {
   log.info("cDownloading launch data...")
@@ -10,10 +19,18 @@ async function downloadLaunchData() {
   }
 
   const launchData = await response.json();
-  console.log(launchData);
+ 
+  for (const launch of launchData) {
+    const flightData = {
+      flightNumber: launch["flight_number"],
+      mission: launch["mission_name"],
+    }
+    launches.set(flightData.flightNumber, flightData) 
+    log.info(JSON.stringify(flightData))
+
+  }
 }
 
 await downloadLaunchData();
 
 log.info("Finished")
-log.critical("We are going down!!!!")
